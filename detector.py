8afa86a394b1d46c240a219916def538264d0283
Chris Archibald <carchi8py@gmail.com>
2020-12-13 09:07:26 -0800

    First round

---
+++ detector.py
@@ -0,0 +1,32 @@
+from gpiozero import MotionSensor
+from picamera import PiCamera
+from datetime import datetime
+
+class Detector(object):
+    def __init__(self):
+        self.pir = MotionSensor(4, threshold=0.5)
+        self.camera = PiCamera()
+        
+    def start(self):
+        while True:
+            self.pir.wait_for_motion()
+            print("Motion detect!")
+            self.start_camera()
+            self.pir.wait_for_no_motion()
+            print("No Motion")
+            self.camera.stop_recording()
+        
+    def start_camera(self):
+        datename = "{0:%Y}-{0:%m}-{0:%d}:{0:%H}:{0:%M}:{0:%S}".format(datetime.now())
+        filename = str(datename) + "video.h264"
+        self.camera.resolution = (640, 480)
+        self.camera.rotation = 180
+        self.camera.start_recording(filename)
+    
+def main():
+    obj = Detector()
+    obj.start()
+    
+
+if __name__ == "__main__":
+    main()
