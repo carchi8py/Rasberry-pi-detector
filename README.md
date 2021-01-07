28e80080a0b43210ce0dd6231eec78d8b6a846c0
Chris Archibald <carchi8py@gmail.com>
2021-01-07 15:52:21 -0800

    updates

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Rasberry-pi-detector
+# Raspberry-pi-detector
+## Goals
+This is a project i am doing for fun to play around with a raspberry pi and some of amazon services.
+## Getting Started
+## Authors
+## Acknowledgments
+The initial bit of code for this project come from the Raspberry pi Parent Detector Project
+https://projects.raspberrypi.org/en/projects/parent-detector
--- detector.py
+++ detector.py
@@ -2,12 +2,19 @@ from gpiozero import MotionSensor
 from picamera import PiCamera
 from datetime import datetime
 
+
 class Detector(object):
+    """
+    Raspberry pi code to detect motion and record video and images
+    """
     def __init__(self):
         self.pir = MotionSensor(4, threshold=0.5)
         self.camera = PiCamera()
         
     def start(self):
+        """
+        When motion is dected start camera until motion is no longer detected
+        """
         while True:
             # The camera always start off detecting motion, so wait until it see no motion
             self.pir.wait_for_no_motion()
@@ -19,12 +26,16 @@ class Detector(object):
             self.camera.stop_recording()
         
     def start_camera(self):
+        """
+        Record video
+        """
         datename = "{0:%Y}-{0:%m}-{0:%d}:{0:%H}:{0:%M}:{0:%S}".format(datetime.now())
         filename = str(datename) + "video.h264"
         self.camera.resolution = (1920, 1080)
         self.camera.rotation = 180
         self.camera.start_recording(filename)
-    
+
+
 def main():
     obj = Detector()
     obj.start()
